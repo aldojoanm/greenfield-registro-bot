0656f8dad54888c35d2f417c2716f9e4f1967e0a
aldojoanm <aldojoanm@gmail.com>
2025-09-16 19:11:04 -0400

    Actualización color

--- quote-pdf.js
+++ quote-pdf.js
@@ -10,7 +10,7 @@ const BRAND = {
   cyan:   '#46acc4',
 };
 const TINT = {
-  headerPurple: '#9AA4AE', // morado un poco más fuerte para el encabezado
+  headerPurple: '#BFC6CE', // morado un poco más fuerte para el encabezado
   rowPurple:    '#F2F4F6', // morado muy suave para TODAS las filas de la tabla
   totalBlue:    '#E9C46A', // celeste muy suave para los "Total USD/Bs"
 };
